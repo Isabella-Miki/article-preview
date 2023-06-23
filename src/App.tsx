efa011651535354edb24d810185b465668d552ec
Isabella Kobayachi <isabellamiki2004@gmail.com>
2023-06-23 17:17:46 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import drawer from './Components/assets/drawers.jpg'
+import womanAvatar from './Components/assets/avatar-michelle.jpg'
+import ShareButton from './Components/ShareButton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='mainCard'>
+
+        <img src={drawer} alt="drawer" className='drawer' />
+
+        <div className='textSection'>
+          <h1>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
+          <p>Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I've got some simple tips to help you make any room feel complete</p>
+
+
+          <div className='author'>
+            <img src={womanAvatar} alt="" className='authorImg' />
+            <div className='aboutAuthor'>
+
+              <p className='name'>Michelle Appleton</p>
+              <p className='date'>28 Jun 2020</p>
+            </div>
+
+            <ShareButton />
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/ShareButton/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import style from './ShareButton.module.scss'
+import { useState } from 'react'
+import iconShare from '../assets/icon-share.svg'
+import iconFacebook from '../assets/icon-facebook.svg'
+import iconPinterest from '../assets/icon-pinterest.svg'
+import iconTwitter from '../assets/icon-twitter.svg'
+
+export default function ShareButton() {
+    const [showShare, setShowShare] = useState(false)
+    const handleClick = () => {
+        setShowShare(!showShare);
+        console.log("clicado")
+    }
+
+    return (
+        <div onClick={() => handleClick()} className={style.shareButton}>
+            <img src={iconShare} alt="" />
+
+            {showShare ?
+                <div className={style.shareActive}>
+                    <div className={style.triangle}></div>
+
+                    <div className={style.socialMedias}>
+                        <p>SHARE</p>
+                        <img src={iconFacebook} alt="" />
+                        <img src={iconTwitter} alt="" />
+                        <img src={iconPinterest} alt="" />
+                    </div>
+                </div> : ' '}
+        </div>
+    )
+}
